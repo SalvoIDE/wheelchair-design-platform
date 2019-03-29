77a5cb8d388771b62a706f098280a403a3452622
Marianne <marilangrand@gmail.com>
2019-03-29 11:42:33 +0100

    as

--- wheelchair/subscribe_gatt_proximity.py
+++ wheelchair/subscribe_gatt_proximity.py
@@ -39,7 +39,7 @@ def handle_proximity_data(handle, value_bytes):
     """
     print("Received data: %s (handle %d)" % (str(value_bytes), handle))
     values = [float(x) for x in value_bytes.decode('utf-8')]
-    find_or_create("chacha",
+    find_or_create("surf-wheel-proximity-b6f1",
                    PropertyType.ONE_DIMENSION).update_values(values)
 
 
