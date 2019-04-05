68b0be352c7e3f65661063ff7f28d35b9adf8b1a
Marianne <marilangrand@gmail.com>
2019-04-05 10:16:25 +0200

    while

--- wheelchair/subscribe_gatt_proximity_rotation.py
+++ wheelchair/subscribe_gatt_proximity_rotation.py
@@ -118,7 +118,7 @@ def check_tiredness():
     print("Checking tiredness!")
 
     # if nobody is behind
-    if proximity_value < 440:
+    while proximity_value < 370:
         # nobodybehind = True
         print("Nobody behind, user self pushing")
         # reseted_value += dif_prev_rotation
