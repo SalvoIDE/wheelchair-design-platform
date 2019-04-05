613d3fa0b807758965a3b231e492b1ab62724b00
Marianne <marilangrand@gmail.com>
2019-04-05 11:10:42 +0200

    a

--- wheelchair/subscribe_gatt_proximity_rotation.py
+++ wheelchair/subscribe_gatt_proximity_rotation.py
@@ -130,7 +130,7 @@ def check_tiredness():
         # nudged = True
         #delete this after
     # if someone is pushing them
-    else:
+    if proximity_value > 300:
         # nobodybehind = False
         # reseted_value = 0
         print("User being pushed, reset rotations")
