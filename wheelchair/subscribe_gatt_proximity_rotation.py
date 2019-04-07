a1a83cd2e7bc88b0271930bf76dd9dded7ed1e96
Marianne <marilangrand@gmail.com>
2019-04-07 21:43:47 +0200

    m

--- wheelchair/subscribe_gatt_proximity_rotation.py
+++ wheelchair/subscribe_gatt_proximity_rotation.py
@@ -84,7 +84,7 @@ def serial_proximity_values():
 
         if proximity_value[0] < 100:
             check_tiredness()
-        else
+        else:
             global reseted_value
             reseted_value = 0
             ser.write('0'.encode())
