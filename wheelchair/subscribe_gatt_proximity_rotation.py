d1fb3c6c5523ce5dd118260cdf70de18c39f6899
Marianne <marilangrand@gmail.com>
2019-04-07 22:32:51 +0200

    a

--- wheelchair/subscribe_gatt_proximity_rotation.py
+++ wheelchair/subscribe_gatt_proximity_rotation.py
@@ -69,7 +69,7 @@ def serial_proximity_values():
         # Split the string using commas as separator, we get a list of strings
         proximity_value = proximity_value_str.split(',')
         # Use the first element of the list as property id
-        proximity = float(proximity_value_str)
+        proximity = proximity_value_str
         # property_id = proximity_value.pop(0)
         # # Get the property from the thing
         # prop = my_thing.properties[property_id]
@@ -83,7 +83,7 @@ def serial_proximity_values():
         # else:
         #     print('Warning: unknown property ' + prox_property_id)
         # # # Finally, we call this method again
-        if proximity [0] < 100:
+        if proximity < 100:
             # check_tiredness()
             # ser.write('1'.encode())
             print("1 sent")
